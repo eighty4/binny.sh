c09eafaab511afeed4c472a36c42e967463d4058
Adam McKee <adam.be.g84d@gmail.com>
2024-12-02 14:41:00 -0600

    db table for script history

--- sql/v001-init-schema.sql
+++ sql/v001-init-schema.sql
@@ -1,24 +1,30 @@
-drop schema if exists install_sh cascade;
-
-create schema install_sh;
-
 create table install_sh.users
 (
     id           bigint primary key,
-    email        varchar   not null,
-    access_token varchar   not null,
     created_when timestamp not null default now(),
-    authed_when  timestamp not null default now()
+    last_visit   timestamp not null default now()
 );
 
 create table install_sh.scripts
 (
     id               bigserial primary key,
-    user_id          integer   not null references install_sh.users (id),
+    user_id          bigint    not null references install_sh.users (id),
     repo_owner       varchar   not null,
     repo_name        varchar   not null,
+    template_spec    varchar   not null,
     template_version varchar   not null,
     created_when     timestamp not null default now(),
     generated_when   timestamp not null default now(),
     constraint script_user_repo_key unique (user_id, repo_owner, repo_name)
+);
+
+create table install_sh.script_history
+(
+    id               bigserial primary key,
+    user_id          bigint    not null references install_sh.users (id),
+    repo_owner       varchar   not null,
+    repo_name        varchar   not null,
+    release_tag      varchar   not null,
+    template_version varchar   not null,
+    generated_when   timestamp not null default now()
 );
