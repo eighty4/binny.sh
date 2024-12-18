8bc8545b6e30cb08cc797065c15e6049d0fb0cfc
Adam McKee <adam.be.g84d@gmail.com>
2024-12-18 02:02:42 -0600

    eleganate the back button

--- frontend/src/components/BackButton.ts
+++ frontend/src/components/BackButton.ts
@@ -10,9 +10,17 @@ export default class BackButton extends HTMLElement {
         return `<template id="${this.TEMPLATE_ID}"><style>${css}</style>${html}</template>`
     }
 
-    static readonly SIZE = 30
+    #div: HTMLDivElement
 
-    static observedAttributes = ['label']
+    #enabled: boolean = false
+
+    #mouseMoveTimeout: any
+
+    #mouseMoveRest: any
+
+    #navCounter: number = 0
+
+    #poppedBackNav: boolean = false
 
     readonly #shadow: ShadowRoot
 
@@ -20,32 +28,70 @@ export default class BackButton extends HTMLElement {
         super()
         this.#shadow = this.attachShadow({mode: 'open'})
         this.#shadow.appendChild(cloneTemplate(BackButton.TEMPLATE_ID))
+        this.#div = this.#shadow.querySelector('div')!
     }
 
     connectedCallback() {
-        const label = this.getAttribute('label')
-        if (label) {
-            this.#setLabel(label)
-        }
-
         this.addEventListener('click', this.#onClick)
+        window.addEventListener('hashchange', this.#onHashchange)
+        window.addEventListener('popstate', this.#onPopstate)
     }
 
     disconnectedCallback() {
         this.removeEventListener('click', this.#onClick)
+        window.removeEventListener('hashchange', this.#onHashchange)
+        window.removeEventListener('popstate', this.#onPopstate)
+        window.removeEventListener('mousemove', this.#onGlobalMouseMove)
     }
 
-    attributeChangedCallback(name: string, _oldValue: string, newValue: string) {
-        if (name === 'label') {
-            this.#setLabel(newValue)
+    #onHashchange = () => {
+        if (this.#poppedBackNav) {
+            if (!--this.#navCounter) {
+                this.#enable(false)
+            }
+        } else {
+            this.#navCounter++
+            this.#enable(true)
         }
     }
 
-    #onClick = () => {
-        history.back()
+    // todo Safari? Firefox?
+    #onPopstate = (e: PopStateEvent) => {
+        this.#poppedBackNav = !!e.state
     }
 
-    #setLabel(label: string) {
-        console.log(label)
+    #onGlobalMouseMove = () => {
+        if (this.#mouseMoveRest) {
+            return
+        }
+        if (this.#mouseMoveTimeout) {
+            clearTimeout(this.#mouseMoveTimeout)
+        } else {
+            this.#div.classList.add('hint')
+        }
+        this.#mouseMoveTimeout = setTimeout(() => {
+            this.#div.classList.remove('hint')
+            this.#mouseMoveTimeout = null
+            this.#mouseMoveRest = setTimeout(() => {
+                this.#mouseMoveRest = null
+            }, 500)
+        }, 300)
+    }
+
+    #onClick = () => history.back()
+
+    #enable(enable: boolean) {
+        if (this.#enabled !== enable) {
+            if (enable) {
+                window.addEventListener('mousemove', this.#onGlobalMouseMove)
+                this.#div.classList.add('enabled')
+            } else {
+                this.#div.classList.remove('enabled')
+                window.removeEventListener('mousemove', this.#onGlobalMouseMove)
+                clearTimeout(this.#mouseMoveTimeout)
+                this.#div.classList.remove('hint')
+            }
+            this.#enabled = enable
+        }
     }
 }
